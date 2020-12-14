588c5713be43a82b2d2b824e30208ea715a4fde1
alex <alex@thinkpad>
2020-12-14 10:39:23 +0100

    QEMU install: updated command for installing Brew on Mac
    hg-changeset: b57f7cdfb9e86023fc709731690ab4a3faafbb07
    hg-branch: qemu

--- contrib/qemu/install.sh
+++ contrib/qemu/install.sh
@@ -238,7 +238,7 @@ if [ $(uname) == "Darwin" ]; then
     # brew is "The missing package manager for macOS"
     # https://brew.sh
     if ! brew -v &> /dev/null; then
-        ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
 
     packages="python2 python3 wget mercurial xz grep pkg-config glib automake libtool pixman mtools"
