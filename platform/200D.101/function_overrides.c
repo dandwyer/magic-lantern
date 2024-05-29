e225d2c67646ddc16e034efd7de613b31494dd17
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2024-05-29 01:39:50 +0100

    200D: fsuPartDec, move stubs -> func_overrides.c
    This will be cleaned up in the future, for now, this change
    is for allowing testing of installer.  Several cams use this pattern
    and duplicate code, it's going to get moved into a shared file.

--- platform/200D.101/function_overrides.c
+++ platform/200D.101/function_overrides.c
@@ -12,14 +12,57 @@
 #include <lens.h>
 #include <tasks.h>
 #include <edmac.h>
-
 #include "fps-engio_per_cam.h"
+
+struct chs_entry
+{
+    uint8_t head;
+    uint8_t sector; //sector + cyl_msb
+    uint8_t cyl_lsb;
+}__attribute__((packed));
+
+struct partition
+{
+    uint8_t  state;
+    struct   chs_entry start;
+    uint8_t  type;
+    struct   chs_entry end;
+    uint32_t start_sector;
+    uint32_t size;
+}__attribute__((aligned,packed));
+
+struct partition_table
+{
+    uint8_t  state; // 0x80 = bootable
+    uint8_t  start_head;
+    uint16_t start_cylinder_sector;
+    uint8_t  type;
+    uint8_t  end_head;
+    uint16_t end_cylinder_sector;
+    uint32_t sectors_before_partition;
+    uint32_t sectors_in_partition;
+}__attribute__((packed));
+
+void fsuDecodePartitionTable(void *partIn, struct partition_table *pTable){
+    struct partition *part = (struct partition *)partIn;
+    pTable->state = part->state;
+    pTable->type = part->type;
+    pTable->start_head = part->start.head;
+    pTable->end_head = part->end.head;
+    pTable->sectors_before_partition = part->start_sector;
+    pTable->sectors_in_partition = part->size;
+
+    //tricky bits - TBD
+    pTable->start_cylinder_sector = 0;
+    pTable->end_cylinder_sector = 0;
+}
 
 void LoadCalendarFromRTC(struct tm *tm)
 {
     _LoadCalendarFromRTC(tm, 0, 0, 16);
 }
 
+#ifndef CONFIG_INSTALLER
 extern struct task* first_task;
 int get_task_info_by_id(int unknown_flag, int task_id, void *task_attr)
 {
@@ -35,6 +78,7 @@ int get_task_info_by_id(int unknown_flag, int task_id, void *task_attr)
     struct task *task = first_task + (task_id & 0xffff);
     return _get_task_info_by_id(task->taskId, task_attr);
 }
+#endif
 
 void SetEDmac(unsigned int channel, void *address, struct edmac_info *ptr, int flags)
 {
