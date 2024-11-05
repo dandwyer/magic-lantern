dd9b512f6c571b1646d4e57c4a0105ac304ae066
stephen-e <33672591+reticulatedpines@users.noreply.github.com>
2024-11-05 23:20:05 +0000

    6d2: enable screenshots and crash logs
    Also has the SD autotune stub found, but there seems
    little point in enabling it, stock speeds are good.

--- platform/6D2.111/features.h
+++ platform/6D2.111/features.h
@@ -1,17 +1,11 @@
-//#define CONFIG_HELLO_WORLD
-
 #define FEATURE_VRAM_RGBA
 
-// Don't Click Me menu looks to be intended as a place
-// for devs to put custom code in debug.c run_test(),
-// and allowing triggering from a menu context.
+#define FEATURE_SCREENSHOT
+//#define FEATURE_SD_AUTOTUNE // exists, but doesn't seem to improve over stock speeds
+
 //#define FEATURE_DONT_CLICK_ME
 
-// prevent ML attempting stack unwinding in some cases.
-// This does not yet work (assumes ARM, not Thumb).  Alex recommends
-// a good looking fix:
-// http://www.mcternan.me.uk/ArmStackUnwinding/
-#undef CONFIG_CRASH_LOG
+#define CONFIG_CRASH_LOG
 
 #undef CONFIG_ADDITIONAL_VERSION
 #undef CONFIG_PROP_REQUEST_CHANGE
